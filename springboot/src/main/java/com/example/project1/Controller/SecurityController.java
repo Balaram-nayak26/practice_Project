424e9d3d44bb0d7ac2cc42304ddc73ee09a81026
Balaram-nayak26 <balaram.nayak@argusoft.com>
2025-08-09 14:08:30 +0530

    implemented spring security

---
+++ springboot/src/main/java/com/example/project1/Controller/SecurityController.java
@@ -0,0 +1,20 @@
+package com.example.project1.Controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("/")
+    public String homePage(HttpServletRequest request){
+        return "This is my spring security home page" +request.getSession().getId();
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
---
+++ springboot/src/main/java/com/example/project1/Repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.example.project1.Repo;
+
+import com.example.project1.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+}
